22b08f766328d6d2543c69e8adde027f091e1ba1
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-12-20 22:19:12 +0530

    Time: 6 ms (69.55%), Space: 13.2 MB (41.36%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int f(string s1,string s2,int i,int j,vector<vector<int>> &dp){
+       if(i<0&&j<0) return 0;
+       if(j<0) return i+1;
+       if(i<0) return j+1; 
+       if(dp[i][j]!=-1) return dp[i][j];
+       if(s1[i]==s2[j]) return dp[i][j]=0+f(s1,s2,i-1,j-1,dp);
+       return dp[i][j]=1+min({f(s1,s2,i,j-1,dp),f(s1,s2,i-1,j,dp),f(s1,s2,i-1,j-1,dp)});
+    }
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        //tabulation
+        for(int j=0;j<=m;j++) dp[0][j]=j;
+        for(int i=0;i<=n;i++) dp[i][0]=i;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                dp[i][j]=0;
+                if(word1[i-1]==word2[j-1]) dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=1+min({dp[i][j-1],dp[i-1][j],dp[i-1][j-1]});
+            }
+        }
+        return dp[n][m];
+    }
+};
