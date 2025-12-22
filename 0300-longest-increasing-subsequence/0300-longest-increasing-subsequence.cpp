fd0461721df19570b73279644cf82424ce5543ab
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-12-22 11:32:16 +0530

    Time: 0 ms (100%), Space: 14.1 MB (87.52%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    int f(vector<int>& nums,int i,int prevIdx,vector<vector<int>>&dp){
-        if(i==nums.size()) return 0;
-        if(dp[i][prevIdx+1]!=-1) return dp[i][prevIdx+1];
-        int l=0;
-        if(prevIdx==-1||nums[prevIdx]<nums[i]){
-            l=1+f(nums,i+1,i,dp);
-        }
-        l=max(l,f(nums,i+1,prevIdx,dp));
-        return dp[i][prevIdx+1]=l;
-    }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size()+1,vector<int>(nums.size()+2,-1));
-        return f(nums,0,-1,dp);
+        vector<int> arr;
+        for(int i=0;i<nums.size();i++){
+            if(i==0) arr.push_back(nums[i]);
+            else{
+                if(arr[arr.size()-1]<nums[i]) arr.push_back(nums[i]);
+                else arr[lower_bound(arr.begin(),arr.end(),nums[i])-arr.begin()]=nums[i];
+            }
+        }
+        return arr.size();
     }
 };
