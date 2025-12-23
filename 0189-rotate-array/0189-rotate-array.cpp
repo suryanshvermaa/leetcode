55b6fb69892067ee23b94615e419b7ae33d6b42b
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-12-23 18:51:08 +0530

    Time: 0 ms (100%), Space: 29.7 MB (37.88%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,17 +4,21 @@ public:
         // Brute force solution
         int n=nums.size();
         k=k%n;
-        vector<int> temp;
-        if(k==0) return;
-        for(int i=n-1;i>=n-k;i--){
-            temp.push_back(nums[i]);
-        }
-        for(int i=n-k-1;i>=0;i--){
-            nums[i+k]=nums[i];
-        }
-        for(int i=k-1;i>=0;i--){
-            nums[i]=temp[k-i-1];
-        }
+        // vector<int> temp;
+        // if(k==0) return;
+        // for(int i=n-1;i>=n-k;i--){
+        //     temp.push_back(nums[i]);
+        // }
+        // for(int i=n-k-1;i>=0;i--){
+        //     nums[i+k]=nums[i];
+        // }
+        // for(int i=k-1;i>=0;i--){
+        //     nums[i]=temp[k-i-1];
+        // }
+        // optimized version
+        reverse(nums.begin(),nums.end()-k);
+        reverse(nums.end()-k,nums.end());
+        reverse(nums.begin(),nums.end());
         return;
     }
 };
